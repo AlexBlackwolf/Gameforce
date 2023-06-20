93e081937ad269c136075621985bbb11e8a263e9
AlexBlackwolf <alex_fortini@hotmail.com>
2023-06-20 14:46:05 +0200

    Project started

---
+++ Gamefroce/src/main/java/com/example/Gamefroce/GamefroceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gamefroce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamefroceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamefroceApplication.class, args);
+	}
+
+}
