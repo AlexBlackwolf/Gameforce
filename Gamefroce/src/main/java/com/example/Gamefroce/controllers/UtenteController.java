2322df8eba940153098f08312f300ca6c536db16
Salvo <pellegrino.salvatore02@gmail.com>
2023-06-22 22:04:51 +0200

    fatta prova di un metodo insert

--- Gamefroce/src/main/java/com/example/Gamefroce/controllers/UtenteController.java
+++ Gamefroce/src/main/java/com/example/Gamefroce/controllers/UtenteController.java
@@ -1,7 +1,10 @@
 package com.example.Gamefroce.controllers;
 
+import com.example.Gamefroce.entity.Utente;
 import com.example.Gamefroce.services.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,10 @@ public class UtenteController {
 
     @Autowired
     public UtenteService uService;
+
+    @GetMapping("/insert")
+    public ResponseEntity<?> insertUtente(Utente u){
+        uService.insertUtente(u);
+        return ResponseEntity.ok().build();
+    }
 }
--- Gamefroce/src/main/java/com/example/Gamefroce/services/UtenteService.java
+++ Gamefroce/src/main/java/com/example/Gamefroce/services/UtenteService.java
@@ -1,5 +1,6 @@
 package com.example.Gamefroce.services;
 
+import com.example.Gamefroce.entity.Utente;
 import com.example.Gamefroce.repository.UtenteRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class UtenteService {
 
     @Autowired
     public UtenteRepo uRepo;
+
+    public void insertUtente (Utente u){
+        uRepo.save(u);
+    }
 }
