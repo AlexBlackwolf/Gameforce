a36f6f51f2a9917821343d68e21f50d7001391b1
AlexBlackwolf <alex_fortini@hotmail.com>
2023-06-22 04:36:28 +0200

    Modified Pom.xml, added videogioco Entity class, added maven dependency and modified properties

---
+++ Gamefroce/src/main/java/Entity/Videogioco.java
@@ -0,0 +1,26 @@
+package Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Videogioco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idVideogioco;
+    private String titolo;
+    private String genere;
+    private String piattaforma;
+    private double prezzo;
+    private double valutazione;
+    private String descrizione;
+    private String requisitiDiSistema;
+
+    // Constructors (including default constructor)
+
+    // Getters and Setters
+
+    // Other methods (if any)
+}
