34fb31f97ec88270ab717032454ffcb5693c050d
AlexBlackwolf <alex_fortini@hotmail.com>
2023-06-23 11:53:49 +0200

    modified Videogioco entity

--- Gamefroce/src/main/java/com/example/Gamefroce/entity/Videogioco.java
+++ Gamefroce/src/main/java/com/example/Gamefroce/entity/Videogioco.java
@@ -21,4 +21,72 @@ public class Videogioco {
     // Getters and Setters
 
     // Other methods (if any)
+
+    public Videogioco() {
+        this.idVideogioco = idVideogioco;
+    }
+
+    public Long getIdVideogioco() {
+        return idVideogioco;
+    }
+
+    public void setIdVideogioco(Long idVideogioco) {
+        this.idVideogioco = idVideogioco;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    public String getPiattaforma() {
+        return piattaforma;
+    }
+
+    public void setPiattaforma(String piattaforma) {
+        this.piattaforma = piattaforma;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double getValutazione() {
+        return valutazione;
+    }
+
+    public void setValutazione(double valutazione) {
+        this.valutazione = valutazione;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getRequisitiDiSistema() {
+        return requisitiDiSistema;
+    }
+
+    public void setRequisitiDiSistema(String requisitiDiSistema) {
+        this.requisitiDiSistema = requisitiDiSistema;
+    }
 }
