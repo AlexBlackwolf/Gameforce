58c1165b8f0643e26d7a4aa521d26f79a0576528
Arabongo <edosurfer118@gmail.com>
2023-06-30 14:23:00 +0200

    fixed carrello

--- Gamefroce/src/main/java/com/example/Gameforce/entity/Carrello.java
+++ Gamefroce/src/main/java/com/example/Gameforce/entity/Carrello.java
@@ -14,14 +14,24 @@ public class Carrello {
     //chiamare ID
     private String id;
     private int codiceCarrello;
+    private String createdOn;
+    private String createBy;
+    private String modifyBy;
+    private String modifyOn;
+
+
     //aggiungere 4 colonne aggiuntive created by, created on, modified by, modified on
     //aggiungere cancellazione logica
     //usare WHERE per vedere i record attivi
     //aggiungere auditable entity
     //add
-    public Carrello(String id, int codiceCarrello) {
+    public Carrello(String id, int codiceCarrello, String createdOn, String createBy, String modifyBy, String modifyOn) {
         this.id = id;
         this.codiceCarrello = codiceCarrello;
+        this.createdOn = createdOn;
+        this.createBy = createBy;
+        this.modifyBy = modifyBy;
+        this.modifyOn = modifyOn;
     }
 
     public String getId() {
@@ -40,5 +50,36 @@ public class Carrello {
         this.codiceCarrello = codiceCarrello;
     }
 
+    public String getCreatedOn() {
+        return createdOn;
     }
+
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(String modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public String getModifyOn() {
+        return modifyOn;
+    }
+
+    public void setModifyOn(String modifyOn) {
+        this.modifyOn = modifyOn;
+    }
+}
 
