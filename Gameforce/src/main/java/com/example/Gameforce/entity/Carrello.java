6378123d465ca6b3ce2f297fe11dc40d52236c23
Arabongo <edosurfer118@gmail.com>
2023-07-13 17:26:00 +0200

    Modified CarrelloService

--- Gameforce/src/main/java/com/example/Gameforce/entity/Carrello.java
+++ Gameforce/src/main/java/com/example/Gameforce/entity/Carrello.java
@@ -8,24 +8,18 @@ import java.util.List;
 @Entity
 @Table(name = "carrello")
 public class Carrello extends AuditableEntity{
-    //finire mapping entity e fare repository
-    //chiamare ID
     private int codiceCarrello;
-
-
 
     @OneToMany(mappedBy = "carrello")
     @JsonManagedReference(value = "ordini-carrello")
     private List<Ordine> ordiniCarello;
 
-    //aggiungere 4 colonne aggiuntive created by, created on, modified by, modified on
-    //aggiungere cancellazione logica
-    //usare WHERE per vedere i record attivi
-    //aggiungere auditable entity
-    //add
     public Carrello(Long id, int codiceCarrello, String createdOn, String createdBy, String modifiedBy, String modifiedOn) {
         super(id, createdOn, createdBy, modifiedBy, modifiedOn);
         this.codiceCarrello = codiceCarrello;
+    }
+    public Carrello(){
+        super();
     }
 
     public int getCodiceCarrello() {
